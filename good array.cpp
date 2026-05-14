f8df4923f38340dfb2ad169ffb94804a15fa1019
JANITHA MUVVALA <janithamuvvala@gmail.com>
2026-05-14 21:10:06 +0530

    Implement isGood function in Solution class

---
+++ good array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isGood(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        if (nums[n-1] != n-1) return false;
+        if (nums[n-1] != nums[n-2]) return false;
+        for (int i = 0; i < n-1; i++) {
+            if (nums[i] != i + 1) return false;
+        }
+
+        return true;
+    }
+};
